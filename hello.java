fbbfe1cd983cd9795ba828ec7ebd531a9aae541a
tajwidahmad <tajwidahmad5@gmail.com>
2025-03-13 06:30:23 +0000

    added

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+
+    public static void main(String[] args) {
+        String x ="munnu";
+        String y ="hello";
+        System.out.println(x.equals(y));
+        System.out.println(x.charAt(x.length()-1));
+        System.out.println((int)x.charAt(x.length()-1));
+    }
+}
